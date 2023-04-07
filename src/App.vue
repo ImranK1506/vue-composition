582bb2b21174a34cf6945083067b9c7103de5279
Imran Khan <m_imran_khan@me.com>
2023-04-07 18:34:03 +0200

    fix route to home page

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink :to="{ name: 'event-details' }">Events</RouterLink> |
+          <RouterLink :to="{ name: 'event-list' }">Events</RouterLink> |
           <RouterLink :to="{ name: 'about' }">About</RouterLink>
         </nav>
       </div>
